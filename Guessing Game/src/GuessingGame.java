5e0a55906b35170617fd7ac52383f996b80f64f4
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-25 13:15:13 -0400

    First Commit

---
+++ Guessing Game/src/GuessingGame.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class GuessingGame 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name, user?");
+		String name = input.next();
+		System.out.print("Welcome, " + name + ". How old are you?");
+		int age = input.nextInt();
+		if (age > 0 && age <= 30)
+		{
+			System.out.print("You are young");	
+		}
+		else if (age > 0 && age <= 60)
+		{
+			System.out.print("You are middle aged");
+		}
+		else if (age > 60)
+		{
+			System.out.print("You are old");
+		}
+		guessTheNumber();
+	}
+
+	public static void guessTheNumber()
+	{
+		(int)num = ((Math.random()*10) + 1);
+		
+	}
+}
